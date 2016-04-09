b1ff332bb663029820cc015354708b5b8a84d662
Steve Y <steve.yorke@gmail.com>
2016-04-09 15:06:00 -0700

    added login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,45 +2,75 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('user', {
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: { isEmail: true }
-    },
-    salt: {
-      type: DataTypes.STRING
-    },
-    password_hash: {
-      type: DataTypes.STRING
-    },
-    password: {
-      type: DataTypes.VIRTUAL,
-      allowNull: false,
-      validate: { len: [7, 100] },
-      set: function(value) {
-        var salt = bcrypt.genSaltSync(10);
-        var hashedPassword = bcrypt.hashSync(value, salt);
-
-        this.setDataValue('password', value);
-        this.setDataValue('salt', salt);
-        this.setDataValue('password_hash', hashedPassword);
-      }
-    }
-  }, {
-    hooks: {
-      beforeValidate: function(user, options) {
-        if (typeof user.email  === 'string') {
-          user.email = user.email.toLowerCase();
-        }
-      }
-    },
-    instanceMethods: {
-      toPublicJSON: function() {
-        var json = this.toJSON();
-        return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-      }
-    }
-  });
+	var user = sequelize.define('user', {
+   	email: {
+	      type: DataTypes.STRING,
+	      allowNull: false,
+	      unique: true,
+	      validate: {
+				isEmail: true
+			}
+		},
+		salt: {
+      	type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+	   	type: DataTypes.VIRTUAL,
+	      allowNull: false,
+	      validate: {
+				len: [7, 100]
+	      },
+	      set: function(value) {
+				// generate random string (salt)
+				var salt = bcrypt.genSaltSync(10);
+				// attach salt to password and encrypt
+				var hashedPassword = bcrypt.hashSync(value, salt);
+				// store these values to DB
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
+	      }
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (user && bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							resolve(user);
+						} else {
+							return reject();
+						}
+					}, function(err) {
+						return reject();
+					});
+				});
+			}
+		},
+		instanceMethods: {
+			// define what info is safe for public exposure
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			}
+		}
+	});
+	return user;
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,31 +12,33 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.send('Todo API root.')
+	res.send('Todo API root.')
 });
 
 // GET /todos?completed=boolean&q=string
 // (if no params, GET all)
 app.get('/todos', function(req, res) {
-  var query = req.query;
-  var where = {};
+	var query = req.query;
+	var where = {};
 
-  if (query.hasOwnProperty('completed') && query.completed === 'true') {
-    where.completed = true;
-  } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
-    where.completed = false;
-  }
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	}
   if (query.hasOwnProperty('q') && query.q.length > 0) {
-    where.description = {
-      $like: '%'+ query.q + '%'
-    };
-  }
+		where.description = {
+	      $like: '%' + query.q + '%'
+		};
+	}
 
-  db.todo.findAll({where: where}).then(function(todos) {
-    res.json(todos);
-  }).catch(function(err) {
-    res.status(500).send();
-  });
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}).catch(function(err) {
+		res.status(500).send();
+	});
 });
 
 // GET /todos/:id
@@ -44,101 +47,114 @@ app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
   db.todo.findById(todoId).then(function(todo) {
-    if (!!todo) { // !! returns truthy/falsey
-      return res.json(todo.toJSON());
-    } else {
-      return res.status(404).send();
-    }
-  }).catch(function(err) {
-    return res.status(500).json(err);
-  })
+	  if (!!todo) { // !! returns truthy/falsey
+		  return res.json(todo.toJSON());
+	  } else {
+		  return res.status(404).send();
+	  }
+	}).catch(function(err) {
+		return res.status(500).json(err);
+	})
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  // use _.pick to sanitize req.body object
-  var body = _.pick(req.body, 'description', 'completed');
+	// use _.pick to sanitize req.body object
+	var body = _.pick(req.body, 'description', 'completed');
 
-  db.todo.create({
-    description: body.description,
-    completed: body.completed
-  }).then(function(todo) {
-    return res.json(todo.toJSON());
-  }).catch(function(err) {
-    return res.status(400).json(err);
-  })
+	db.todo.create({
+		description: body.description,
+		completed: body.completed
+	}).then(function(todo) {
+		return res.json(todo.toJSON());
+	}).catch(function(err) {
+		return res.status(400).json(err);
+	})
 });
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
-  var todoId = parseInt(req.params.id, 10);
-  var matched = db.todo.findById(todoId);
+	var todoId = parseInt(req.params.id, 10);
+	var matched = db.todo.findById(todoId);
 
-  db.todo.destroy({
-    where: {
-      id: todoId
-    }
-  }).then(function(affectedRows) {
-    if (affectedRows === 0) {
-      res.status(404).json({
-        error: 'No todo with that id'
-      });
-    } else {
-      res.status(204).send();
-    }
-  }).catch(function(err) {
-    res.status(500).send();
-  })
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(affectedRows) {
+		if (affectedRows === 0) {
+			res.status(404).json({
+				error: 'No todo with that id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}).catch(function(err) {
+		res.status(500).send();
+	})
 });
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
-  var todoId = parseInt(req.params.id, 10);
-  var body = _.pick(req.body, 'description', 'completed');
-  var attrs = {};
+	var todoId = parseInt(req.params.id, 10);
+	var body = _.pick(req.body, 'description', 'completed');
+	var attrs = {};
 
-  if (body.hasOwnProperty('description')) {
-    attrs.description = body.description;
-  }
-  if (body.hasOwnProperty('completed')) {
-    attrs.completed = body.completed;
-  }
+	if (body.hasOwnProperty('description')) {
+		attrs.description = body.description;
+	}
+	if (body.hasOwnProperty('completed')) {
+		attrs.completed = body.completed;
+	}
 
-  db.todo.findById(todoId)
-  .then(function(todo) {
-    if (todo) {
-      return todo.update(attrs)
-        .then(function(todo){
-          res.json(todo.toJSON());
-        }, function(err) {
-          res.status(400).json(err);
-        });
-    } else {
-      res.status(404).json({
-        error: 'No todo with that id.'
-      });
-    }
-  }, function() {
-    res.status(500).send();
-  })
+	db.todo.findById(todoId)
+		.then(function(todo) {
+			if (todo) {
+				return todo.update(attrs)
+					.then(function(todo) {
+						// res.json(todo.toJSON());
+					}, function(err) {
+			      res.status(400).json(err);
+				});
+			} else {
+				res.status(404).json({
+					error: 'No todo with that id.'
+				});
+			}
+		}, function() {
+			res.status(500).send();
+		})
 });
 
 // POST /users
 app.post('/users', function(req, res) {
-  var body = _.pick(req.body, 'email', 'password');
+	var body = _.pick(req.body, 'email', 'password');
 
-  db.user.create({
-    email: body.email,
-    password: body.password
-  }).then(function(user) {
-    return res.json(user.toPublicJSON());
-  }).catch(function(err) {
-    return res.status(400).json(err);
-  })
+	db.user.create({
+		email: body.email,
+		password: body.password
+	}).then(function(user) {
+		return res.json(user.toPublicJSON());
+	}).catch(function(err) {
+		return res.status(400).json(err);
+	})
 });
 
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function(){
-    console.log('Express listening on port ' + PORT + '!');
-  });
+// POST /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function(user) {
+		res.json(user.toPublicJSON());
+	}, function() {
+		// intentionally vague error (for security reasons)
+		res.status(401).send();
+	});
+
+});
+
+db.sequelize.sync({force: true}).then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
